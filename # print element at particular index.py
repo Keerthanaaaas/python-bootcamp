9439d684274b9fd54e1dddf140ca814970f67bf3
Keerthanaaaas <165894927+Keerthanaaaas@users.noreply.github.com>
2024-07-25 19:51:37 +0530

    Add files via upload

---
+++ # print element at particular index.py
@@ -0,0 +1,5 @@
+# print element at particular index
+k=int(input())
+list=list(map(int,input().split(" "))) 
+m=k%len(list)
+print(list[m-1])
---
+++ sum of sqrs of given number.py
@@ -0,0 +1,6 @@
+k=int(input())
+s=0
+while k>0:
+    s=s+(k%10)**2
+    k=int(k/10)
+print(s)
